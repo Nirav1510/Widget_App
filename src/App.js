b71b9db87b279ba2a674525c52865f5551c4d383
Nirav <gusainirav1@gmail.com>
2021-07-11 23:01:13 +0530

    removing unneccessary files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Accordian from "./components/Accordion";
 
-function App() {
+const items = [
+  {
+    title: "What is React?",
+    content: "It is a front end javascript Framework",
+  },
+  {
+    title: "Why react is so popular?",
+    content:
+      "ReactJS has become highly popular because of its extra simplicity and flexibility.",
+  },
+  {
+    title: "Why react hooks are bad?",
+    content:
+      "React uses lint rules and will throw errors to try to prevent developers from violating this detail of Hooks. ",
+  },
+];
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordian items={items} />
     </div>
   );
-}
+};
 
 export default App;
