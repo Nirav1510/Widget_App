5d624f32a2a0b0c9af72ac3944c751eee021232c
Nirav <gusainirav1@gmail.com>
2021-07-15 12:55:14 +0530

    Translate widget is ready

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Dropdown from './Dropdown';
-
+import Convert from './Convert';
 const options = [
   {
     label: 'Afrikaans',
@@ -13,6 +13,10 @@ const options = [
   {
     label: 'Hindi',
     value: 'hi',
+  },
+  {
+    label: 'Dutch',
+    value: 'nl',
   },
 ];
 
@@ -34,8 +38,12 @@ const Translate = () => {
         onSelectedChange={setLanguage}
         options={options}
       />
+      <hr />
+      <h3 className="ui header">Output</h3>
+      <Convert text={text} language={language} />
     </div>
   );
 };
 
 export default Translate;
+//AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM
